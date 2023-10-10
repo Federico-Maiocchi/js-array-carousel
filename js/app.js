3723183ebf2760202cdf847e7362024616b6219c
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-10 16:31:14 +0200

    ADD: link array and cicle for

--- js/app.js
+++ js/app.js
@@ -5,6 +5,27 @@ const arrayCarousel = [
     "./img/02.webp",
     "./img/03.webp",
     "./img/04.webp",
-    "./img/05.webp",
-    
+    "./img/05.webp" 
 ]
+
+const galleryDomElement = document.querySelector('.gallery');
+console.log(galleryDomElement);
+
+console.log(arrayCarousel)
+
+for (let i = 0; i < arrayCarousel.length; i++) {
+
+    // console.log(i);
+
+    const currentSrc = arrayCarousel[i];
+    console.log(currentSrc);
+    
+    const htmlString = `
+    <div>
+        <img src="${currentSrc}">
+    </div>
+    `
+
+    console.log(htmlString);
+    galleryDomElement.innerHTML += htmlString;
+}
